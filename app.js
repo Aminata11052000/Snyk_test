fee922b83320ab43cd3735a99568f7a7ddf299e0
Aminata11052000 <baaminata075@gmail.com>
2024-11-28 23:59:31 -0800

    Welcome page added

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const userRouter = require('./routers/user.router');
-
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors()); // Autorise toutes les origines
-
 app.use(body_parser.json());
+app.use('/', userRouter);
 
-app.use('/',userRouter);
+// Configurer EJS comme moteur de template
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Middleware d'erreur global
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
-  });
-  
+});
 
 module.exports = app;
-
